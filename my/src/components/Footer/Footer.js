11d43584f17a7d907a305c5d01e2b1b533343a40
jarell Boone <jarellboone4@gmail.com>
2021-04-28 10:33:05 -0400

    created the react app and implemented the components

---
+++ my/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Footer/Footer.css'
+
+
+
+
+export default function Footer() {
+    return (
+<footer>
+        <a href="https://www.linkedin.com/in/jarell-boone-90b14b1ba?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BQp10fUbfR6OHPOj99ffFWg%3D%3D"
+            style="font-size: 50px;"   class="fa fa-linkedin"></a>
+    
+           
+            <a href="https://github.com/JarellB4"style="font-size:50px" id="Hub" class="fa">&#xf09b;</a>
+    </footer>
+
+    )}
+
---
+++ my/src/components/Projects/Projects.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+
+    
+ <div class="container w-75">
+ <div class="row">
+     <div class="col-12 col-md-6">
+         <Strong id="Lkb">Looking for Balance</Strong>
+         <br>
+         <a href="https://jarellb4.github.io/Looking-for-balance/">Looking for balance link</a>
+
+         <a href="https://github.com/JarellB4/Looking-for-balance.git">Jarell Boone"s Github Repository link</a>
+         <img src="./Screenshot (2).png" alt="Lookin for balance">
+     </div>
+
+     <div class="col-12 col-md-6">
+         <Strong id="Pwg">Password Generator</Strong>
+         <br>
+         <a href="https://jarellb4.github.io/PassWGenJb.4/">Password Generator</a>
+     <a href="https://github.com/JarellB4/PassWGenJb.4">Github Password</a>
+    <img src="./Screenshot (4).png" alt="Password Generator"> 
+ </div>
+     <div class="col-12 col-md-6">
+        <Strong id="Cr"> Burger App </Strong>
+        <br>
+        <a href="https://burgerheaven.herokuapp.com/">Burger app</a>
+        <a href="https://github.com/JarellB4/jarellb4.github.io">Github Burger App </a>
+         <img src="./Screenshot (10).png" alt="">
+     </div>
+     <div id="Res" class="col-12 col-md-6">
+         <Strong id="dr">dayrec</Strong>
+      
+         <a href="https://dayrec.herokuapp.com/">DayRec </a>
+         <img src="./Screenshot (12).png" alt="">
+     </div>
+     <div class="col-12 col-md-6">
+         <img src="" alt="">
+
+     </div>
+
+ </div>
+
+
+
+
+
+
+
+)}
